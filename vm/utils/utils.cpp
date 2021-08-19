0ad288d32a12dbb9f0337d2db03ab771ec96e059
amsen20 <amsen@aut.ac.ir>
2021-08-19 15:04:56 +0430

    Add initiate main in vm.

--- vm/utils/utils.cpp
+++ vm/utils/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.h"
 
+#include <iostream>
+
 int32_t
 __4bytes_to_int(unsigned char *ptr) {
     int32_t ret = 0;
@@ -7,4 +9,18 @@ __4bytes_to_int(unsigned char *ptr) {
         ret = ret << 8 | ptr[i];
     
     return ret;
+}
+
+void
+finish(std::string msg){
+    std::cerr << msg << "\n";
+    exit(0);
+}
+
+template<typename ...Args>
+void
+ensuref(bool cond, Args&&... msg){
+    if(cond)
+        return;
+    finish(get_str(msg...));
 }
--- vm/utils/utils.h
+++ vm/utils/utils.h
@@ -2,8 +2,16 @@
 #define __UTILS_H__
 
 #include <stdint.h>
+#include <string>
 
 int32_t
 __4bytes_to_int(unsigned char *ptr);
+
+void 
+finish(std::string msg);
+
+template<typename ...Args>
+void
+ensuref(bool cond, Args&&... msg);
 
 #endif
