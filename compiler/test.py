bb6a502b193d52ff929c3f52522bd88a1f91b519
amsen20 <amsen@aut.ac.ir>
2021-08-30 19:15:05 +0430

    Ignore test.py.

--- compiler/test.py
+++ compiler/test.py
@@ -1,16 +1,2 @@
-def ali(a, b):
-<<<<<<< HEAD
-    return a
-=======
-    if(a < b):
-        a = 2 * a
-        b = a * b
-        return a
-    return b
-<<<<<<< HEAD
->>>>>>> fa80dfd203d98236dc3273bda0ead8bea3d9476d
-=======
-
 def main(a, b):
-    return ali(a, b)
->>>>>>> 0ad288d32a12dbb9f0337d2db03ab771ec96e059
+    return 2*a+b
